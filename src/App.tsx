2d13da6ed0c0ca04731d83163c217d426d69c58a
WilkerSC2 <wilkersantos777@gmail.com>
2025-12-12 15:12:54 -0300

    Added package.json

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Site Luana</h1>
+      </header>
+      <main>
+        <p>Bem-vindo ao site!</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
